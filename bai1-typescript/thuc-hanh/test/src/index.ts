f1426fdfc2a93279b58e38739046e5508b54d05f
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-10-01 09:35:29 +0700

    hai commit

---
+++ bai1-typescript/thuc-hanh/test/src/index.ts
@@ -0,0 +1,49 @@
+// const list: number[] = [1, 2, 3];
+//
+// const categories: Array<string> =
+//     ['Sport', 'IT', 'Car'];
+//
+// console.log('list');
+// list.forEach((num) =>
+//     console.log(num.toFixed(2))
+// );
+//
+// console.log('categries');
+// categories.forEach((str) =>
+//     console.log(str.includes('a'))
+// );
+//
+// const listSquare = list.map(num => num*num);
+// console.log(listSquare)
+// // Output: [1, 4, 9]
+//
+// // lọc các phần tử thỏa mãn
+// const result = categories.filter(str => str.length > 2)
+// console.log(result);
+// // Output: ['Sport', 'Car']
+
+/**
+ * Tuple
+ */
+
+// Declare a tuple type
+let x: [string, number, string];
+// Initialize it
+x = ["hello", 10, "end"]; // OK
+// Initialize it incorrectly
+// x = [10, "hello"]; // Error
+
+console.log(x[0].substr(0)); // OK
+console.log(x[1]);
+console.log(x[2]);
+// Error, Property 'substr' does not exist on type 'number'.
+
+x[2] = "world";
+console.log(x[0].substr(0)); // OK
+console.log(x[1]);
+console.log(x[2].charAt(2));
+// OK, 'string' can be assigned to 'string | number'
+// x[5] = 100.25;
+// console.log(x[5].toString());
+// // OK, 'string' and 'number' both have 'toString'
+
---
+++ bai1-typescript/thuc-hanh/typescript-demo/dist/fundamentals.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./scss/styles.scss");
+function main() {
+    console.log("START");
+    if (true) {
+        var lang = "vi";
+        var target = "en-us";
+        console.log("inside block");
+        console.log(target);
+    }
+    console.log(lang);
+}
+main();
+//# sourceMappingURL=fundamentals.js.map
