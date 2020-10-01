e6c2f6381762e383f9f3ec1307f79d41c336ffb6
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-10-01 17:01:38 +0700

    commit bai hacker-new

---
+++ bai4-angular-component/thuchanh/hacker-new/src/app/like/like.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-like',
+  templateUrl: './like.component.html',
+  styleUrls: ['./like.component.css']
+})
+export class LikeComponent implements OnInit {
+  likes = 0;
+  // tslint:disable-next-line:typedef
+  likeThis(){
+    this.likes ++;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
