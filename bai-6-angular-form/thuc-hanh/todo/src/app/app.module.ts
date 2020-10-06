244acf2135d5c90f3193f93e9b5bf4d5b52a0b4e
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-10-06 09:21:13 +0700

    bai 6 todo

---
+++ bai-6-angular-form/thuc-hanh/todo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TodoComponent } from './todo/todo.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent
+  ],
+    imports: [
+        BrowserModule,
+        NgbModule,
+        ReactiveFormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
